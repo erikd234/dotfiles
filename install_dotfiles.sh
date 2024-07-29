3141fd4813d43bfe2c2031bd9110dd84ce06cc56
erikd234 <erikd234@gmail.com>
2024-07-28 22:35:09 -0700

    update isntall script

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -4,12 +4,13 @@ pushd $HOME
 echo ".cfg" >> .gitignore
 mkdir .cfg
 git clone https://github.com/erikd234/dotfiles.git --bare $HOME/.cfg
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-config config --local status.showUntrackedFiles no
+echo "alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'" >> $HOME/.bashrc
+source .bashrc
+/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME config --local status.showUntrackedFiles no
 #Places the files in the work-tree
 #If you see errors for this , delete the files then run
 # config checkout until the files are deleted
 # DONT run the whole script again I dont know what will happen
-config checkout
+/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout
 popd
 
